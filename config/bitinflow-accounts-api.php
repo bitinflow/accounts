8f962d1e068e34f00fc5aee8f8c7341b06038228
envoyr <hello@envoyr.com>
2022-01-02 14:04:01 +0100

    Add sso user provider

--- config/bitinflow-accounts-api.php
+++ config/bitinflow-accounts-api.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'client_id'     => env('BITINFLOW_ACCOUNTS_KEY', ''),
-    'client_secret' => env('BITINFLOW_ACCOUNTS_SECRET', ''),
-    'redirect_url'  => env('BITINFLOW_ACCOUNTS_REDIRECT_URI', ''),
-    'base_url'      => env('BITINFLOW_ACCOUNTS_BASE_URI', ''),
+    'client_id' => env('BITINFLOW_ACCOUNTS_KEY'),
+    'client_secret' => env('BITINFLOW_ACCOUNTS_SECRET'),
+    'redirect_url' => env('BITINFLOW_ACCOUNTS_REDIRECT_URI'),
+    'base_url' => env('BITINFLOW_ACCOUNTS_BASE_URI'),
 ];
